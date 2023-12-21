a8580796262d7e9b8ae54e81b667872c32758c71
belyaev-ay <anton.belyaev@sibit.tech>
2023-12-21 19:41:45 +0700

    Empty spring project

---
+++ src/main/java/pw/avvero/springlogbackjavaconfiguration/SpringLogbackJavaConfigurationApplication.java
@@ -0,0 +1,11 @@
+package pw.avvero.springlogbackjavaconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLogbackJavaConfigurationApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLogbackJavaConfigurationApplication.class, args);
+	}
+}
---
+++ src/test/java/pw/avvero/springlogbackjavaconfiguration/SpringLogbackJavaConfigurationApplicationTests.java
@@ -0,0 +1,11 @@
+package pw.avvero.springlogbackjavaconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLogbackJavaConfigurationApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
